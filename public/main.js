0099d0e9957647d009b98cb55212d15decf06995
Edan Sneh <edan.sneh@gmail.com>
2018-08-26 18:21:54 -0700

    working canvas

--- public/main.js
+++ public/main.js
@@ -1,19 +1,30 @@
 var canvas, ctx;
 
-function init(){
+function sclick(ele){
+    if(event.key === 'Enter') {
+        init(ele.value)
+        ele.style.display = "none";
+    }
+}
+
+function init(name){
 
     canvas = document.getElementById('gameCanvas');
     canvas.style.width = window.innerWidth + 'px';
     canvas.style.height = window.innerHeight + 'px';
     ctx = canvas.getContext('2d');
-
+    
     console.log(canvas.style.width);
     console.log(canvas.style.height);
-
     ctx.beginPath();
+    ctx.arc(100,75,50,0,2*Math.PI);
+    ctx.stroke();
     ctx.moveTo(0,0);
     ctx.lineTo(window.innerWidth, window.innerHeight);
-    ctx.stroke();
+    ctx.beginPath();
 }
 
-window.onload = init;
+// function component(width, height, color, x, y, type) {
+//     this.type = type;
+//     this.
+// }
